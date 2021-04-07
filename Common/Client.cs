3a6f0636aef2b29957794735180e9a2e53e7f1a4
webhideaway <sasharadulovic@hotmail.com>
2021-04-07 21:36:51 +0100

    common

--- Common/Client.cs
+++ Common/Client.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,16 @@ namespace Common
 {
     public class Client
     {
+        public static async Task RunAsync()
+        {
+            var clientSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Connecting to port 8087");
+
+            clientSocket.Connect(new IPEndPoint(IPAddress.Loopback, 8087));
+            var stream = new NetworkStream(clientSocket);
+
+            await Console.OpenStandardInput().CopyToAsync(stream);
+        }
     }
 }
--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -12,14 +12,7 @@ namespace TcpEcho
     {
         static async Task Main(string[] args)
         {
-            var clientSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-
-            Console.WriteLine("Connecting to port 8087");
-
-            clientSocket.Connect(new IPEndPoint(IPAddress.Loopback, 8087));
-            var stream = new NetworkStream(clientSocket);
-
-            await Console.OpenStandardInput().CopyToAsync(stream);
+            await Common.Client.RunAsync();
         }
     }
 }
