6105db34fb551c167d87bd780b19f679c140533f
webhideaway <sasharadulovic@hotmail.com>
2021-05-24 21:39:18 +0100

    testing

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -9,6 +9,8 @@ namespace Server
     {
         static async Task Main(string[] args)
         {
+            var random = new Random();
+
             var localEndPoint = new IPEndPoint(IPAddress.Loopback, 1212);
 
             Console.WriteLine("Server listening for requests on local end point: {0}", localEndPoint);
@@ -17,6 +19,7 @@ namespace Server
 
             server.RegisterHandler<Person, Person>(request =>
             {
+                Task.Delay(random.Next(1000, 5000));
                 if (request.Age < 18) throw new Exception($"Under aged person {request.Name}");
                 return new Person(request.Name.ToLowerInvariant(), request.Age * -1);
 
@@ -24,6 +27,7 @@ namespace Server
 
             server.RegisterHandler<Car, Car>(request =>
             {
+                Task.Delay(random.Next(1000, 5000));
                 if (request.Age < 3) throw new Exception($"Under aged car {request.Brand}");
                 return new Car(request.Brand.ToLowerInvariant(), request.Age * -1);
             });
